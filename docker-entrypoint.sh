55583b436d5473853214145e86966112e0499a01
Cristian Camargo <56233205+cfcamargo@users.noreply.github.com>
2026-01-06 00:45:48 -0300

    commit seed

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,15 +7,15 @@ pnpm prisma generate
 echo "🗃️  Prisma migrate deploy..."
 pnpm prisma migrate deploy
 
-if pnpm -s run | grep -q "db:seed"; then
-  echo "🌱 Prisma db seed (npm script db:seed)..."
-  pnpm run db:seed
-elif [ -f "prisma/seed.ts" ] || [ -f "prisma/seed.js" ]; then
-  echo "🌱 Prisma db seed..."
-  pnpm prisma db seed
-else
-  echo "⚠️  Seed não configurado - pulando."
-fi
+# if pnpm -s run | grep -q "db:seed"; then
+#   echo "🌱 Prisma db seed (npm script db:seed)..."
+#   pnpm run db:seed
+# elif [ -f "prisma/seed.ts" ] || [ -f "prisma/seed.js" ]; then
+#   echo "🌱 Prisma db seed..."
+#   pnpm prisma db seed
+# else
+#   echo "⚠️  Seed não configurado - pulando."
+# fi
 
 echo "🚀 Start API..."
 exec node dist/src/main.js
